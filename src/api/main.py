389a4de553e91cf396d7f12a5b5f74d4a2917f34
Frédéric Vincent <frederic.vincent@protonmail.com>
2025-01-30 19:18:56 +0100

    main.py : gestion de favicon.ico

---
+++ src/api/main.py
@@ -0,0 +1,24 @@
+
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+import os
+
+app = FastAPI()
+
+# Monte le dossier static
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Route pour servir le favicon
+@app.get('/favicon.ico', include_in_schema=False)
+async def favicon():
+    return FileResponse(os.path.join('static', 'favicon.ico'))
+
+
+#----------------------------------------------------------------#
+# Point de terminaison pour vérifier que l'API est fonctionnelle #
+#----------------------------------------------------------------#
+    
+@app.get('/verify')
+def get_verify():
+    return {"message": "L'API est fonctionnelle."}
