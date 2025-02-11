ca7998bbd07ebde3fddb2cdbba3f8c74b24c2da9
Frédéric Vincent <frederic.vincent@protonmail.com>
2025-02-11 14:36:08 +0100

    [setup.py] Mise à jour du fichier

--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
 from setuptools import setup, find_packages
 
+# Lire requirements.txt
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
     name="MLOps_LondonFireBrigade",
     version="0.1",
-    packages=find_packages()
+    author="Frédéric Vincent",
+    author_email="frederic.vincent@protonmail.com",
+    description="Un projet MLOps pour la London Fire Brigade",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=required,
+    python_requires=">=3.8",  # version minimale de Python requise
 )
+
